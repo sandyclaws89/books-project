457a60326a41a5388569fb99b422f226690b18bc
sandyclaws89 <96080101+sandyclaws89@users.noreply.github.com>
2022-05-29 21:25:21 +0200

    impostazione progetto

---
+++ database/migrations/2022_05_29_174740_create_books_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBooksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('books', function (Blueprint $table) {
+            $table->id();
+            $table->string('title', 250);
+            $table->binary('cover_image');
+            $table->string('author', 250)->nullable();
+            $table->decimal('price');
+            $table->string('genre', 150)->nullable();
+            $table->text('synopsis')->nullable();
+            $table->float('review')->nullable();
+            $table->string('edition', 100)->nullable();
+            $table->string('publishing_house', 250)->nullable();
+            $table->date('pubblication_date')->nullable();
+            $table->date('checkin_date');
+            $table->boolean('available');
+            $table->tinyInteger('quantity')->unsigned();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('books');
+    }
+}
---
+++ database/seeds/BookSeeder.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\Book;
+use Faker\Generator as Faker;
+
+
+class BookSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(Faker $faker)
+    {
+        $faker ->addProvider(new \Faker\Provider\it_IT\Person($faker));
+
+        for ($i=1; $i <= 40; $i++) {
+            $bookData = [
+                'title'                 =>  $faker->sentence(rand(1, 4)),
+                'cover_image'           =>  'https://picsum.photos/id/' . rand(0, 1084) . '/640/480',
+                'author'                =>  $faker->name(),
+                'price'                 =>  $faker->randomFloat(1, 4, 150),
+                'genre'                 =>  $faker->randomElement(['Horror', 'Dramatic', 'Romantic', 'Essay', 'Adult','Historical']),
+                'synopsis'              =>  $faker->paragraph(),
+                'review'                =>  $faker->randomFloat(1, 0, 5),
+                'edition'               =>  $faker->words(rand(1, 5), true),
+                'publishing_house'      =>  $faker->words(rand(1, 4), true),
+                'pubblication_date'     =>  $faker->date(),
+                'checkin_date'          =>  $faker->date(),
+                'available'             =>  $faker->boolean(),
+                'quantity'              =>  $faker->numberBetween(1, 199),
+            ];
+            $book = new Book();
+            $book->fill($bookData);
+            $book->save();
+        }
+    }
+}
